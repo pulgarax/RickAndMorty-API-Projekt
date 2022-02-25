2aa40916bdc0c433bc1e3a87fef4bbdfa719334e
Axel Pulgar <brepundtrep@yahoo.com.ar>
2022-02-25 10:51:32 +0100

    add apiurl

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 import './style.css'
 
+const apiurl = 'https://rickandmortyapi.com/api/character'
+
+fetchCharacters();
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch(apiurl);
+    const data = await response.json();
+    console.log(data.results);
+  } 
+  catch(error) {
+    console.log(error);
+  } 
+}
+
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
