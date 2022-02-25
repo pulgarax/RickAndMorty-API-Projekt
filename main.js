1a73a4962d30d72f2fb16dd9bfe659d7309086ef
Niklas Röttger <niklas.roettger@outlook.de>
2022-02-25 11:51:35 +0100

    adding first dom manipulation

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
-import './style.css'
+import "./style.css";
 
-const apiurl = 'https://rickandmortyapi.com/api/character'
+const apiurl = "https://rickandmortyapi.com/api/character";
 
 fetchCharacters();
+
+let cards = [];
+
+const cardsContainer = document.querySelector("[data-js=cards]");
 
 async function fetchCharacters() {
   try {
     const response = await fetch(apiurl);
     const data = await response.json();
-    console.log(data.results);
-  } 
-  catch(error) {
+    cardsContainer.innerHTML = "";
+    data.results.forEach((card) => {
+      const cardElement = document.createElement("article");
+      cardElement.className = "Card";
+      cardElement.innerHTML = `
+    <img src="${card.image}"/>
+      <section class="Card__information">
+        <h2>${card.name}</h2>
+        <p>${card.status} - ${card.species}</p>
+        <h3 class="Card__information-headline">Last known location:</h3>
+        <p>${card.location.name}</p>
+        <h3 class="Card__information-headline">First seen in:</h3>
+        <p>${card.episode[0]}</p>
+      </section>
+      `;
+      cardsContainer.append(cardElement);
+    });
+  } catch (error) {
     console.log(error);
-  } 
+  }
 }
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
